be162b9a8e91f0141a8a17e911fc597a05ca8ef5
akifcorduk <akifcorduk@gmail.com>
2026-02-12 03:46:11 -0800

    presolved

--- benchmarks/linear_programming/cuopt/mip_test_instances.hpp
+++ benchmarks/linear_programming/cuopt/mip_test_instances.hpp
@@ -1,17 +1,17 @@
 /* clang-format off */
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright (c) 2022-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 /* clang-format on */
 #pragma once
 #include <string>
 #include <vector>
-std::vector<std::string> instances = {"polygonpack4-7_1.mps",
-                                      "polygonpack4-7_2.mps",
-                                      "polygonpack4-7_3.mps",
-                                      "neos-3355120-tarago_1.mps",
-                                      "neos-3355120-tarago_2.mps",
-                                      "neos-3355120-tarago_3.mps",
-                                      "liu_1.mps",
-                                      "liu_2.mps"};
+std::vector<std::string> instances = {"1hour-1_model.mps",
+                                      "1hour-2_model.mps",
+                                      "huge2_model.mps",
+                                      "large_model.mps",
+                                      "med-quick_model.mps",
+                                      "medium_model.mps",
+                                      "small_model.mps",
+                                      "smallish_model.mps"};
