c8ead44aad0ed4080890082ef0cb1434651431ae
akifcorduk <akifcorduk@gmail.com>
2026-04-07 05:10:05 -0700

    add instances

--- benchmarks/linear_programming/cuopt/mip_test_instances.hpp
+++ benchmarks/linear_programming/cuopt/mip_test_instances.hpp
@@ -1,17 +1,25 @@
 /* clang-format off */
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright (c) 2022-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 /* clang-format on */
 #pragma once
 #include <string>
 #include <vector>
-std::vector<std::string> instances = {"polygonpack4-7_1.mps",
-                                      "polygonpack4-7_2.mps",
-                                      "polygonpack4-7_3.mps",
-                                      "neos-3355120-tarago_1.mps",
-                                      "neos-3355120-tarago_2.mps",
-                                      "neos-3355120-tarago_3.mps",
-                                      "liu_1.mps",
-                                      "liu_2.mps"};
+std::vector<std::string> instances = {"Maersk/260225w-jimmy.mps",
+                                      "Maersk/260225w.mps",
+                                      "Maersk/BLOT_FULL.mps",
+                                      "Maersk/PLP_3964_LP_anon.mps",
+                                      "Maersk/PLP_3964_MILP_anon.mps",
+                                      "Maersk/bao_1.mps",
+                                      "Maersk/bao_2.mps",
+                                      "anaplan/unpresolved/1hour-1_model.mps",
+                                      "anaplan/unpresolved/huge2_model.mps",
+                                      "anaplan/unpresolved/med-quick_model.mps",
+                                      "anaplan/unpresolved/small_model.mps",
+                                      "anaplan/unpresolved/1hour-2_model.mps",
+                                      "anaplan/unpresolved/large_model.mps",
+                                      "anaplan/unpresolved/medium_model.mps",
+                                      "anaplan/unpresolved/smallish_model.mps",
+                                      "bcg/P1.mps"};
